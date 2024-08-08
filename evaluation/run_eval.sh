7cfe26a32f8685fb673bc55773efa2a2e8dc1c56
Min-Seok Kim <106315809+snumin44@users.noreply.github.com>
2024-08-08 14:24:19 +0900

    Update run_eval.sh

--- evaluation/run_eval.sh
+++ evaluation/run_eval.sh
@@ -2,13 +2,10 @@
 
 GPU_ID=0
 CUDA_VISIBLE_DEVICES=$GPU_ID python3 eval.py \
-    	--model '../output/supervised_simcse' \
-        --tokenizer '../output/supervised_simcse' \
-        --test_data 'kor_test' \
+    	--model '../output/unsupervised_simcse' \
+        --tokenizer '../output/unsupervised_simcse' \
+        --test_data 'klue_dev' \
     	--output_path '../output/result.txt' \
         --batch_size 256 \
         --max_length 64 \
         --pooler 'cls' \
-        --padding  \
-        --truncation \
-        --shuffle 
